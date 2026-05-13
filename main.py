ff93a2264da207c16f0c0d329085986cd8525f92
Ananya Vijay <ananyavijay05@gmail.com>
2026-05-13 14:40:53 +0530

    feat(decorator): add IST timestamp, timing, and exception logging to log_action

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from  abc import ABC, abstractmethod
+import time
+from datetime import datetime
 from functools import wraps
+from zoneinfo import ZoneInfo
 
 def log_action(func):
     @wraps(func)
     def wrapper(*args, **kwargs):   
-        print(f"[LOG] Action: '{func.__name__}' started")
-        result = func(*args, **kwargs)
-        print(f"[LOG] Action: '{func.__name__}' completed")
-        return result
+        ist_time = datetime.now(ZoneInfo("Asia/Kolkata")).strftime("%d-%b-%Y %H:%M:%S IST")
+        print(f"[LOG] {ist_time} | Action: '{func.__name__}' started")
+        start = time.time()
+
+        try:
+            result = func(*args, **kwargs)
+            end = time.time()
+            print(f"[LOG] {ist_time} | Action: '{func.__name__}' completed in {end-start}")
+        except Exception as err:
+            print(f"[LOG] {ist_time} | Action: '{func.__name__}'" f"raise {type(err).__name__}: {err}")
+            raise
     return wrapper
 
 class PatientNotFoundError(Exception):
